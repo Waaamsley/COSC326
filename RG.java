3ce35a643dce1eedb7916d5ab1a92a309cdd04be
Waaamsley <walja829@student.otago.ac.nz>
2017-09-19 13:22:20 +1200

    Add files via upload

--- RG.java
+++ RG.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class RG{
 
     private static List<String> colours;
+    private static List<Integer> indexes;
     private static StringBuilder sb;
 
     public static void main(String[] args){
@@ -30,19 +31,37 @@ public class RG{
 
     private static void visualiser(int[] inputs){
         colours = new ArrayList<String>();
+        Integer index;
         sb = new StringBuilder();
         colours.add("G");
         colours.add("G");
-        //int n = inputs[0];
         int r;
         int g;
         int n = inputs[0];
         for (int a = 2; a < (n + inputs[1]); a++){;
+            indexes = new ArrayList<Integer>();
             r = 0;
             g = 0;
-            for (int i = 1; i <= a/2; i++){;
-                if (a % i == 0 || a % i == 1){
-                    if (colours.get(i).equals("G")){
+            for (int i = a/2; i > 1; i--){
+                index = a/i;
+                indexes.add(index);
+            }
+            g++;
+            for (Integer i : indexes){
+                //System.out.println(i);
+            }
+            //System.out.println("-----");
+            for (int i = 0; i < indexes.size(); i++){
+                if (indexes.size() == 1){
+                    r++;
+                }else if(i == 0){
+                    if (colours.get(indexes.get(i)).equals("G")){
+                        g++;
+                    }else{
+                        r++;
+                    }
+                }else if (indexes.get(i-1) < indexes.get(i)){
+                    if (colours.get(indexes.get(i)).equals("G")){
                         g++;
                     }else{
                         r++;
@@ -59,6 +78,7 @@ public class RG{
         for (int a = inputs[0]; a < colours.size(); a++){
             sb.append(colours.get(a));
         }
+        System.out.println(inputs[0] + " " + inputs[1]);
         System.out.println(sb.toString());
     }
     
